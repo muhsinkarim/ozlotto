c283b987cc1b118b2293d8e361845ad1bf307e04
mkarim <muhsinkarim@outlook.com>
2016-12-16 22:00:37 +1100

    division 1 tables

--- ozlotto_wrangling.R
+++ ozlotto_wrangling.R
@@ -1,9 +1,12 @@
 ##### Oz Lotto
 # Quantifying duplicate draws
 
-# Muhsin Karim 1/12/2016
-#--------------------------------------------------------------------------------------------------
+# Muhsin Karim 16/12/2016
 
+# Notes
+# https://en.wikipedia.org/wiki/Oz_Lotto
+# Starting 18 October 2005, a seventh number began to be drawn, lengthening Division 1 odds.
+#--------------------------------------------------------------------------------------------------
 
 #### Libraries
 
@@ -13,88 +16,121 @@ suppressPackageStartupMessages(library(dplyr))
 #### Get data 
 
 setwd('C:/Users/Muhsin Karim/Documents/GitHub/ozlotto')
-dfCurrent <- read.csv('Ozlotto.csv', stringsAsFactors = F)
+df <- read.csv('Ozlotto.csv', stringsAsFactors = F)
 #dfPrevious <- read.csv('OzLotto.txt', stringsAsFactors = F) # Don't use - missing 7th winning draw
+
+
+#### Rename columns
+
+## Select columns for dfCurrent
+df <- df %>%
+    select(
+        draw_number = Format..Draw.Number,
+        date = Draw.Date..yyyymmdd., 
+        winning_1 = Winning.Number.1,     
+        winning_2 = X2,                  
+        winning_3 = X3,
+        winning_4 = X4,
+        winning_5 = X5,
+        winning_6 = X6,  
+        winning_7 = X7,
+        supplementary_1 = Supplementary.1,
+        supplementary_2 = X2.1,
+        division_1 = Division.1,
+        division_2 = X2.2, 
+        division_3 = X3.1, 
+        division_4 = X4.1, 
+        division_5 = X5.1,
+        division_6 = X6.1,
+        division_7 = X7.1
+        )
 
 
 #### Formatting
 
-## Select columns for dfCurrent
-df <- dfCurrent %>%
-    select(
-        Format..Draw.Number,  
-        Draw.Date..yyyymmdd., 
-        Winning.Number.1,     
-        X2,                  
-        X3,
-        X4,
-        X5,
-        X6,  
-        X7,
-        Supplementary.1,
-        X2.1
-        )
+### Date
 
-# ## Merge current and 
-# df <- rbind.data.frame(df, dfPrevious)
-# df <- df[order(df$Format..Draw.Number), ]
-# rownames(df) <- NULL
-
-## Rename columns
-df <- df %>%
-    select(
-        DateNumber = Format..Draw.Number,
-        Date = Draw.Date..yyyymmdd., 
-        Winning1 = Winning.Number.1,     
-        Winning2 = X2,                  
-        Winning3 = X3,
-        Winning4 = X4,
-        Winning5 = X5,
-        Winning6 = X6,  
-        Winning7 = X7, 
-        Supplementary1 = Supplementary.1,
-        Supplementary2 = X2.1
-    )
-
-## Coerce to date
-df$Date <- as.Date(as.character(df$Date), format = '%Y%m%d')
+df$date <- as.Date(as.character(df$date), format = '%Y%m%d')
 
 
-# #### Quantify duplicate draws
-# # For each row, get seven numbers, order then record
+### Division columns as numeric
+
+df2 <- as.data.frame(apply(df[ , 12:18], 2, function(x) gsub("\\$", "", x))) # Remove "$" from Division columns
+df2 <- as.data.frame(apply(df2, 2, function(x) gsub(",", "", x))) # Remove "," from Division columns
+df2 <- data.frame(lapply(df2, as.character), stringsAsFactors = F) # As character
+df2 <- data.frame(lapply(df2, as.numeric)) # As numeric
+
+
+### Bind data frames
+
+df <- cbind.data.frame(df[ , 1:11], df2)
+
+
+#### Percentages of division 1 draws (seven numbers)
+
+## Stack all winning numbers as one data frame column
+all_draws <- c(df$winning_1,
+               df$winning_2,
+               df$winning_3,
+               df$winning_4,
+               df$winning_5,
+               df$winning_6,
+               df$winning_7)
+
+## Build division 1 table
+all_division1_percentages = as.data.frame(table(all_draws))
+
+## Total number of lotto nights
+total_nights <- nrow(df)
+
+## Percentage of number drawn out of all nights
+all_division1_percentages$Percentage <- round(all_division1_percentages$Freq / total_nights * 100, 1)
+all_division1_percentages$all_draws <- as.numeric(all_division1_percentages$all_draws)
+colnames(all_division1_percentages) <- c("winning_draw", "frequency", "percentage")
+
+
+#### Percentages of winning division 1 draws
+
+## Subset winning division 1 draws (prize awarded)
+df_winning_division1 <- df[-which(df$division_1 == 0), ]
+
+## Stack all winning numbers as one data frame column
+all_draws <- c(df_winning_division1$winning_1,
+               df_winning_division1$winning_2,
+               df_winning_division1$winning_3,
+               df_winning_division1$winning_4,
+               df_winning_division1$winning_5,
+               df_winning_division1$winning_6,
+               df_winning_division1$winning_7)
+
+## Build winning division 1 table
+winning_division1_percentages = as.data.frame(table(all_draws))
+
+## Total number of winning division 1 lotto nights
+total_nights <- nrow(winning_division1_percentages)
+
+## Percentage of number drawn out of all nights
+winning_division1_percentages$Percentage <- round(winning_division1_percentages$Freq / total_nights * 100, 1)
+winning_division1_percentages$all_draws <- as.numeric(winning_division1_percentages$all_draws)
+colnames(winning_division1_percentages) <- c("winning_draw", "frequency", "percentage")
+
+
+#### Save tables as CSVs
+
+write.csv(all_division1_percentages, file = "all_division1_percentages.csv", row.names = F)
+write.csv(winning_division1_percentages, file = "winning_division1_percentages.csv", row.names = F)
+
+
+
+
+# #---------------------------------------------------------------------------------------------------------------------------
+# ## Check the probability of winning division 1
+# # 1/45 * 1/44 * 1/43 * 1/42 * 1/41 * 1/40 * 1/39
+# # 45 * 44 * 43 * 42 * 41 * 40 * 39 # 1 in 228,713,284,800
+# # 45 * 44 * 43 * 42 * 41 * 40 * 39 / factorial(7) # 1 in 45,379,620 because order does not matter
 # 
-# df$FirstDivision <- ""
-# 
-# for (i in 1:nrow(df)) {
-#     
-#     ## Get numbers from draws
-#     first_division <- as.numeric(df[i , c("Winning1", "Winning2", "Winning3", "Winning4", "Winning5", "Winning6", "Winning7")])
-#     
-#     ## Order draws
-#     first_division <- sort(first_division)
-# 
-#     ## Record ordered draw
-#     df$FirstDivision[i] <- paste(first_division, collapse=" ")
-# }
-# 
-# 
-# sum(duplicated(df$FirstDivision))
-
-## Stack all draws as one data frame column
-all_draws <- c(as.numeric(df$Winning1), 
-             as.numeric(df$Winning2),
-             as.numeric(df$Winning3),
-             as.numeric(df$Winning4),
-             as.numeric(df$Winning5),
-             as.numeric(df$Winning6),
-             as.numeric(df$Winning7))
-
-t = as.data.frame(table(all_draws))
-names(t)[1] = 'Draw'
-head(t)
-
-library(ggplot2)
-ggplot(t, aes(x = reorder(Draw, Freq), y = Freq)) + 
-    geom_bar(stat = "identity") +
-    coord_flip()
-
+# ## Plot division 1
+# library(ggplot2)
+# ggplot(division1, aes(x = reorder(Draw, Freq), y = Freq)) + 
+#     geom_bar(stat = "identity") +
+#     coord_flip()
